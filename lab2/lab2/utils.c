9dc7beb9210de8653e8340d2e795933082934cef
educunhA04 <118934693+educunhA04@users.noreply.github.com>
2025-02-22 11:39:26 +0000

    utils done

--- lab2/lab2/utils.c
+++ lab2/lab2/utils.c
@@ -4,7 +4,7 @@
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
   if (lsb == NULL) return 1;
-  *lsb = val & 0x00FF;
+  *lsb = val & 0xFF;
   return 0;
 }
 
@@ -15,8 +15,9 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (value == NULL) return 1;
+  uint32_t auxval;
+  if (sys_inb(port, &auxval) != 0) return 1;
+  *value = auxval & 0xFF;
+  return 0;
 }
