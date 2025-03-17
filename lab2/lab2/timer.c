689b26d5559bf1899968cc37dbdec5091ed9a891
educunhA04 <118934693+educunhA04@users.noreply.github.com>
2025-03-17 17:00:28 +0000

    ints

--- lab2/lab2/timer.c
+++ lab2/lab2/timer.c
@@ -4,26 +4,18 @@
 #include <stdint.h>
 
 #include "i8254.h"
-
+int hook_id = 0;
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
+  
   return 1;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
-    /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  return sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id);
 }
 
 int (timer_unsubscribe_int)() {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  return sys_irqrmpolicy(&hook_id);
 }
 
 void (timer_int_handler)() {
@@ -32,15 +24,18 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (st == NULL) return 1;
+  if (timer > 2 || timer < 0) return 1;
+  uint8_t rbc = 0;
+  rbc = (TIMER_RB_CMD | )
+  return 0;
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
+  switch(timer) {
+    
+  }
   printf("%s is not yet implemented!\n", __func__);
 
   return 1;
