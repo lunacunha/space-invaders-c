521e7aabef99312aec82d05311577f381d442172
Rodrigo Araújo <rodrigogomesaraujo04@gmail.com>
2025-03-24 16:30:29 +0000

    initial commit for lab4

---
+++ lab4/i8042.h
@@ -0,0 +1,18 @@
+#ifndef _LCOM_I8042_H_
+#define _LCOM_I8042_H_
+
+#define KEYBOARD_IRQ 1
+
+#define KEYBOARD_STATUS 0x64
+#define KEYBOARD_IN_CMD 0x64
+#define KEYBOARD_OUT_CMD 0x60
+#define KEYBOARD_READ_CMD 0x20
+#define KEYBOARD_WRITE_CMD 0x60
+
+#define KB_BREAK_ESC 0x81
+#define KB_TWO_BYTES 0xE0
+#define KB_MAKE_CODE BIT(7)
+
+#define IN_BUFFER_FULL BIT(1)
+
+#endif /* _LCOM_I8042_H */
