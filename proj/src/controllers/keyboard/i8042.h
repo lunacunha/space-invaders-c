1cd885f571ff054562d8913fe7dd4b9455a1b43b
educunhA04 <118934693+educunhA04@users.noreply.github.com>
2025-05-25 11:51:22 +0100

    some fixes

--- proj/src/controllers/keyboard/i8042.h
+++ proj/src/controllers/keyboard/i8042.h
@@ -9,7 +9,15 @@
 #define KEYBOARD_READ_CMD 0x20
 #define KEYBOARD_WRITE_CMD 0x60
 
+#define KB_A 0x1E
+#define KB_D 0x20
+#define KB_SPACE 0x39
+#define KB_ENTER 0x1C
+#define KB_UP_ARROW 0x48
+#define KB_DOWN_ARROW 0x50
 #define KB_BREAK_ESC 0x81
+
+
 #define KB_TWO_BYTES 0xE0
 #define KB_BREAK_CODE BIT(7)
 
