61a73a3a18eaddd2b05073e5f83ca43f8d76a966
Luna <lunagomescunha@gmail.com>
2025-05-26 15:36:55 +0100

    centered menu

--- proj/src/game/menu/menu.c
+++ proj/src/game/menu/menu.c
@@ -52,37 +52,64 @@ void menu_handle_input() {
 }
 
 void menu_render() {
+    // Get screen dimensions from mode_info
+    uint16_t screen_width = mode_info.XResolution;
+    uint16_t screen_height = mode_info.YResolution;
+    
+    // Define image dimensions (you'll need to adjust these based on your actual XPM sizes)
+    uint16_t title_width = 400;   // Adjust to your title XPM width
+    uint16_t title_height = 200;  // Adjust to your title XPM height
+    uint16_t option_width = 145;  // Based on your XPM comment "145 51"
+    uint16_t option_height = 51;  // Based on your XPM comment "145 51"
+    
     switch (current_state) {
-        case MENU_MAIN:
-            print_xpm(title, 0, 0);
+        case MENU_MAIN: {
+            // Center the title horizontally, position it in upper portion of screen
+            uint16_t title_x = (screen_width - title_width) / 2;
+            uint16_t title_y = screen_height / 4;  // Position at 1/4 from top
+            
+            print_xpm(title, title_x, title_y);
+            
+            // Center the menu options horizontally, position them below title
+            uint16_t options_x = (screen_width - option_width) / 2;
+            uint16_t options_start_y = title_y + title_height + 50;  // 50px gap after title
+            uint16_t option_spacing = option_height + 20;  // 20px spacing between options
             
             switch (current_option) {
                 case MENU_OPTION_START:
-                    print_xpm(start1, 100, 300);  
-                    print_xpm(scores2, 100, 350);  
-                    print_xpm(quit2, 100, 400);    
+                    print_xpm(start1, options_x, options_start_y);  
+                    print_xpm(scores2, options_x, options_start_y + option_spacing);  
+                    print_xpm(quit2, options_x, options_start_y + (option_spacing * 2));    
                     break;
                 case MENU_OPTION_SCORES:
-                    print_xpm(start2, 100, 300);   
-                    print_xpm(scores1, 100, 350);  
-                    print_xpm(quit2, 100, 400);   
+                    print_xpm(start2, options_x, options_start_y);   
+                    print_xpm(scores1, options_x, options_start_y + option_spacing);  
+                    print_xpm(quit2, options_x, options_start_y + (option_spacing * 2));   
                     break;
                 case MENU_OPTION_QUIT:
-                    print_xpm(start2, 100, 300);   
-                    print_xpm(scores2, 100, 350);  
-                    print_xpm(quit1, 100, 400);    
+                    print_xpm(start2, options_x, options_start_y);   
+                    print_xpm(scores2, options_x, options_start_y + option_spacing);  
+                    print_xpm(quit1, options_x, options_start_y + (option_spacing * 2));    
                     break;
                 default:
                     break;
             }
             break;
+        }
             
         case MENU_START:
             break;
             
-        case MENU_SCORES:
-            //print_xpm(score_board, 0, 0);
+        case MENU_SCORES: {
+            // Center the score board
+            uint16_t score_width = 195;   // Based on your XPM comment "195 51"
+            uint16_t score_height = 51;   // Based on your XPM comment "195 51"
+            uint16_t score_x = (screen_width - score_width) / 2;
+            uint16_t score_y = (screen_height - score_height) / 2;
+            
+            print_xpm(score_board, score_x, score_y);
             break;
+        }
             
         case MENU_QUIT:
             current_state = MENU_EXIT;
